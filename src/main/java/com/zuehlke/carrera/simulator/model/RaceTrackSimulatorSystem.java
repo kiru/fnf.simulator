c717ad6cb1af78ede4d713c0db8d02b9f714fa66
Yves Thrier <yves.thrier@zuehlke.com>
2015-08-14 00:21:30 +0200

    Remove duplication and improve handling of clock actor system

--- src/main/java/com/zuehlke/carrera/simulator/model/RaceTrackSimulatorSystem.java
+++ src/main/java/com/zuehlke/carrera/simulator/model/RaceTrackSimulatorSystem.java
@@ -3,13 +3,12 @@ package com.zuehlke.carrera.simulator.model;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import com.zuehlke.carrera.akkautils.AkkaUtils;
-import com.zuehlke.carrera.racetrack.client.RaceTrackToRelayConnection;
 import com.zuehlke.carrera.relayapi.messages.PowerControl;
 import com.zuehlke.carrera.relayapi.messages.RaceStartMessage;
 import com.zuehlke.carrera.relayapi.messages.RaceStopMessage;
 import com.zuehlke.carrera.simulator.config.SimulatorProperties;
 import com.zuehlke.carrera.simulator.model.akka.RaceTrackSimulationActor;
-import com.zuehlke.carrera.simulator.model.akka.clock.GaussianClock;
+import com.zuehlke.carrera.simulator.model.akka.clock.ClockActor;
 import com.zuehlke.carrera.simulator.model.akka.clock.StartClock;
 import com.zuehlke.carrera.simulator.model.akka.clock.StopClock;
 import com.zuehlke.carrera.simulator.model.akka.messages.ActorRegistration;
@@ -23,45 +22,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
-/**
- * Represents a single simulated race track
- */
 public class RaceTrackSimulatorSystem {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RaceTrackSimulatorSystem.class);
-
+    private static final Logger LOG = LoggerFactory.getLogger(RaceTrackSimulatorSystem.class);
     private final String raceTrackId;
-
+    private final PilotInterface connection;
+    private final SimpMessagingTemplate template;
     private ActorSystem simulator;
-
     private ActorRef raceTrackActor;
     private ActorRef clock;
-
-    private final SimpMessagingTemplate template;
-    private final PilotInterface connection;
 
     public RaceTrackSimulatorSystem(String raceTrackId, PilotInterface connection,
                                     SimpMessagingTemplate template, RealDistribution tickDistribution,
                                     SimulatorProperties properties) {
-
-        if(raceTrackId == null) throw new IllegalArgumentException("raceTrackId must not be null");
-        if(template == null) throw new IllegalArgumentException("template must not be null");
-        if(connection == null) throw new IllegalArgumentException("relayConnectorService must not be null");
-
+        if (raceTrackId == null) {
+            throw new IllegalArgumentException("raceTrackId must not be null");
+        }
+        if (template == null)  {
+            throw new IllegalArgumentException("template must not be null");
+        }
+        if (connection == null) {
+            throw new IllegalArgumentException("relayConnectorService must not be null");
+        }
         this.raceTrackId = raceTrackId;
         this.template = template;
         this.connection = connection;
-
         init(tickDistribution, properties);
     }
 
-    private void init( RealDistribution tickDistribution, SimulatorProperties properties ) {
-
-        LOGGER.info("Starting racetrack simulator with mean " + (int)tickDistribution.getNumericalMean() + " ms" );
-
-        simulator = ActorSystem.create ("Simulator");
-        clock = simulator.actorOf(GaussianClock.props( tickDistribution));
-
+    private void init(RealDistribution tickDistribution, SimulatorProperties properties) {
+        LOG.info("Starting racetrack simulator with mean " + (int) tickDistribution.getNumericalMean() + " ms");
+        simulator = ActorSystem.create("Simulator");
+        clock = simulator.actorOf(ClockActor.props(tickDistribution));
         // dispatch the tick to stomp clients
         ActorRef tickDispatcher = simulator.actorOf(StompTickDispatcher.props(template));
         clock.tell(new ActorRegistration(tickDispatcher), ActorRef.noSender());
@@ -80,45 +71,28 @@ public class RaceTrackSimulatorSystem {
         clock.tell(new ActorRegistration(raceTrackActor), ActorRef.noSender());
     }
 
-
-
-    /**
-     *  Starts the clock that ticks the simulator
-     */
-    public void startClock(){
+    public void startClock() {
         // Start the clock which drives the whole simulator
         clock.tell(new StartClock(), ActorRef.noSender());
     }
 
-    /**
-     *   Stops the clock that ticks the simulator
-     */
     public void stopClock() {
         clock.tell(new StopClock(), ActorRef.noSender());
     }
 
-    public void startRace ( RaceStartMessage message ) {
+    public void startRace(RaceStartMessage message) {
         startClock();
         raceTrackActor.tell(message, ActorRef.noSender());
     }
 
-    public void stopRace ( RaceStopMessage message ) {
+    public void stopRace(RaceStopMessage message) {
         stopClock();
         raceTrackActor.tell(message, ActorRef.noSender());
     }
 
-    public void shutdown () {
+    public void shutdown() {
         stopClock();
         simulator.shutdown();
-    }
-
-    /**
-     * ask and the raceTrackActor and...
-     * @return the current track design
-     */
-    public TrackDesign getTrackDesign() {
-
-        return AkkaUtils.askActor(getClass(), TrackDesign.class, raceTrackActor, new QueryTrackDesign());
     }
 
     public void setPower(PowerControl power) {
@@ -133,13 +107,21 @@ public class RaceTrackSimulatorSystem {
         raceTrackActor.tell(new PowerChange(-delta), ActorRef.noSender());
     }
 
-    public void reset () {
+    public void reset() {
         raceTrackActor.tell(new Reset(), ActorRef.noSender());
+    }
+
+    /**
+     * ask and the raceTrackActor and...
+     *
+     * @return the current track design
+     */
+    public TrackDesign getTrackDesign() {
+        return AkkaUtils.askActor(getClass(), TrackDesign.class, raceTrackActor, new QueryTrackDesign());
     }
 
     // select design and make sure we don't return before it's done.
     public TrackDesign selectDesign(String trackDesign) {
         return AkkaUtils.askActor(getClass(), TrackDesign.class, raceTrackActor, new QuerySelectDesign(trackDesign));
-
     }
 }
--- src/main/java/com/zuehlke/carrera/simulator/model/akka/clock/StartClock.java
+++ src/main/java/com/zuehlke/carrera/simulator/model/akka/clock/StartClock.java
@@ -1,7 +1,4 @@
 package com.zuehlke.carrera.simulator.model.akka.clock;
 
-/**
- * Created by paba on 10/24/14.
- */
 public class StartClock {
 }
