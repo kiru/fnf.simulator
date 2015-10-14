19412f3e30918a444125d544dd4fd5a1a5ff142a
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2015-10-14 14:30:46 +0200

    added comment

--- src/main/java/com/zuehlke/carrera/simulator/model/akka/messages/ActorRegistration.java
+++ src/main/java/com/zuehlke/carrera/simulator/model/akka/messages/ActorRegistration.java
@@ -3,6 +3,10 @@ package com.zuehlke.carrera.simulator.model.akka.messages;
 import akka.actor.ActorRef;
 
 /**
+ * Reverse Engineering note:
+ * This class seems to be a marker-wrapper to keep another actor reference.
+ * This "message" is sent, with the intent to add the given actorRef as a subscriber.
+ *
  * Created by wgiersche on 06/09/14.
  */
 public class ActorRegistration {
